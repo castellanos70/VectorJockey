92ecbaac7b4ebd8f7c951954497ffa333c5641a5
VolatileDawn <VolatileDawn@gmail.com>
2021-03-15 22:47:41 -0600

    adding touch

--- src/VectorJockey.js
+++ src/VectorJockey.js
@@ -180,6 +180,9 @@ function init()
     window.addEventListener('mousemove', mouseMove);
     window.addEventListener('mouseup', mouseUp);
     window.addEventListener('wheel', mouseWheel);
+    window.addEventListener('touchstart', function(e){
+        alert(e.changedTouches[0].pageX) // alert pageX coordinate of touch point
+    }, false)
 }
 
 
