26bb5a1d1009a24cf21babf86c5f99f9a686e0ca
Ervan Darnell <edarnell@kelvinist.com>
2021-03-21 17:59:36 -0700

    wrong coordinate in cross product calculation fixed

--- src/Station.js
+++ src/Station.js
@@ -8,7 +8,7 @@ class Coord
    cross (edge) {
       let p1=edge[0].loc;
       let p2=edge[1].loc;
-      return (this.x-p1.x)*(p2.y-p1.y) - (this.y-p1.y)*(p2.x-p1.y)
+      return (this.x-p1.x)*(p2.y-p1.y) - (this.y-p1.y)*(p2.x-p1.x)
    }
 }
 
